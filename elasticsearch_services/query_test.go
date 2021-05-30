104440d4d887497ca3e1664311f4a2ad31f8a478
Himali saini <himalisaini1512@gmail.com>
2021-05-30 14:01:45 +0530

    Test cases updated

--- elasticsearch_services/query_test.go
+++ elasticsearch_services/query_test.go
@@ -17,24 +17,69 @@ func TestConstructQuery(t *testing.T) {
 		want *strings.Reader
 	}{
 
-		{name: "test123",
+		{name: "test101",
 			args: args{query: `{"query":{"match_all":{}}}`},
 			want: strings.NewReader(`{"query":{"match_all":{}}}`),
 		},
 		{name: "test102",
-			args: args{query: `{"quech_all":"hi"}`},
-			want: strings.NewReader(`{"quech_all":"hi"}`),
+			args: args{query: `{
+				"query": {
+				  "nested": {
+					"path": "address",
+					"query": {
+					  "bool": {
+						"must": [
+						  { "match": { "address.city": "Pune" } }
+						]
+					  }
+					},
+					"score_mode": "avg"
+				  }
+				}
+			  }`},
+			want: strings.NewReader(`{
+				"query": {
+				  "nested": {
+					"path": "address",
+					"query": {
+					  "bool": {
+						"must": [
+						  { "match": { "address.city": "Pune" } }
+						]
+					  }
+					},
+					"score_mode": "avg"
+				  }
+				}
+			  }`),
+		},
+
+		{name: "test103",
+			args: args{query: `{
+				"min_score": 1,
+				"query" : {
+					"match" : { "dept" : "Computer Science" }
+				}
+			}`},
+			want: strings.NewReader(`{
+				"min_score": 1,
+				"query" : {
+					"match" : { "dept" : "Computer Science" }
+				}
+			}`),
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			got := ConstructQuery(tt.args.query)
-
 			if !reflect.DeepEqual(got, tt.want) {
 				log.Printf("%T", got)
 				log.Printf("%T", tt.want)
 				t.Errorf("ConstructQuery() got = %v, want %v", got, tt.want)
 			}
+			log.Println("Test Case : ", tt.name, " successfully passed.")
+			println()
+			println()
 
 		})
 	}
